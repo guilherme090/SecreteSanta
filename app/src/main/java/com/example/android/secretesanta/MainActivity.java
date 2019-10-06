26028c67beb4362e769562181b412c7a971c59ff
Guilherme Simoes <guilherme-elec12@hotmail.com>
2019-10-06 20:34:47 -0300

    [Feature]
    Add data structure (collection of people).

--- app/src/main/java/com/example/android/secretesanta/MainActivity.java
+++ app/src/main/java/com/example/android/secretesanta/MainActivity.java
@@ -12,23 +12,31 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     /**
-    Scene declaration for app in chronological order (activity_main is the first one)
+     * Scene declaration for app in chronological order (activity_main is the first one)
      **/
     private Scene activity_main; //initial screen
     private Scene scene_name_email; //screen for adding names and e-mails
 
     /**
-     GUI elements in the app by scene
+     * GUI elements in the app by scene
      **/
     //activity_main
+
     //scene_name_email
 
+    /**
+     * Data structures
+     **/
+    final int MIN_PLAYERS = 3; //it does not make sense to sort 2 people or less
+    final int MAX_PLAYERS = 50; //do not allow more than this many people
+    Person [] listOfParticipants = new Person[MAX_PLAYERS];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Assign app's scenes to what is in the .xml files
         activity_main = Scene.getSceneForLayout((ViewGroup)findViewById(R.id.sceneContainer),
                 R.layout.activity_main,this);
         scene_name_email = Scene.getSceneForLayout((ViewGroup)findViewById(R.id.sceneContainer),
